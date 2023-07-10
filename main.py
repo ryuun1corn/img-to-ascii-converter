050262d882be7a3240fa10811cd8c3549bc9827a
yudaynarif <65879706+ryuun1corn@users.noreply.github.com>
2023-07-10 10:29:29 +0700

    Updated main.py
    Updated the main.py to enable link between configuration file.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from configparser import ConfigParser
 import cv2 as cv
-import math
+
+config = ConfigParser()
+config.read("converter_config.ini")
+config_data =config["DEFAULT"]
 
 def ascii_converter(intensity):
     ascii="@%#*+=-."
@@ -9,8 +13,8 @@ def ascii_converter(intensity):
 path = str(input("Insert the name of file (with extension): "))
 img =cv.imread(path, cv.IMREAD_GRAYSCALE)
 new_width=int(img.shape[1]*0.5)
-new_height=int(img.shape[0]*0.5*0.46)
-min_width=200
+new_height=int(img.shape[0]*0.5*float(config_data["width_to_height_ratio"]))
+min_width=int(config_data["image_width"]) # Variable for the desired image width
 min_height=int(min_width*new_height/new_width)
 smaller = cv.resize(img, (min_width,min_height))
 for i in range(min_height):
